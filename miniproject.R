44aa47b15c211c7ebc6266147edc22299788f875
nkaankutlu <nkaan.kutlu@gmail.com>
2021-10-01 16:46:43 +0300

    Edit

--- miniproject.R
+++ miniproject.R
@@ -11,6 +11,10 @@ DGE.analysis <- function(rawdata, group1, group2, method){
   df <- data
   
   ####creating a DGElist for manipulation and normalization of the count values data####
+  ####creating a DGElist for manipulation and normalization of the count values data####
+  ####creating a DGElist for manipulation and normalization of the count values data####
+  ####creating a DGElist for manipulation and normalization of the count values data####
+  ####creating a DGElist for manipulation and normalization of the count values data####
   
   group <- factor(c(group1, group2))
   y <- DGEList(as.matrix(data), group = group)
